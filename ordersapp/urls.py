afc4439182ab009f80a8f35aca00cb75ef0b9663
IlyaMuralha <muralhakuzmenko@gmail.com>
2021-06-28 00:30:36 +0500

    Create urls orderapp

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -5,4 +5,10 @@ import ordersapp.views as ordersapp
 app_name = 'ordersapp'
 
 urlpatterns = [
+    path('', ordersapp.OrderList.as_view(), name='list'),
+    path('create/<pk>/', ordersapp.OrderCreate.as_view(), name='create'),
+    path('update/<pk>/', ordersapp.OrderUpdate.as_view(), name='update'),
+    path('read/<pk>/', ordersapp.OrderRead.as_view(), name='read'),
+    path('delete/<pk>/', ordersapp.OrderDelete.as_view(), name='delete'),
+    path('forming/complete/pk/', ordersapp.forming_complete, name='forming_complete'),
 ]
